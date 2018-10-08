0bc2871ecef06e8716dfb4eea09c225344eb2a43
Zerouali <oussamazerouali42@gmail.com>
2018-10-08 10:38:51 +0200

    the run application class

---
+++ TemchiB3id/src/main/java/Co_Voiturage/TemchiB3id/App.java
@@ -0,0 +1,18 @@
+package Co_Voiturage.TemchiB3id;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+
+
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(App.class, args);
+
+System.out.println("salam");
+
+	}
+}
