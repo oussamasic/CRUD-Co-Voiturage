af842f4c1a45eda7c844eca0c59b94176b86257d
Zerouali <oussamazerouali42@gmail.com>
2018-10-11 12:47:50 +0200

    New repository

--- TemchiB3id/src/main/java/Co_Voiturage/TemchiB3id/Repository/UserOffreRepositry.java
+++ TemchiB3id/src/main/java/Co_Voiturage/TemchiB3id/Repository/UserOffreRepositry.java
@@ -1,5 +1,7 @@
 package Co_Voiturage.TemchiB3id.Repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
@@ -7,7 +9,9 @@ import Co_Voiturage.TemchiB3id.Model.Offre_User;
 
 public interface UserOffreRepositry extends JpaRepository<Offre_User, Long> {
 	
-
+	List<Offre_User> findByOffre(Long id_offre );
+	List<Offre_User> findByUser(Long id_user );
+	
     // les méthodes save(), findOne(), findAll(), count(), delete() sont generées automatiquement par le JPA
 
 }
