f261808056c4b2c469297b3c240860872c176565
Zerouali <oussamazerouali42@gmail.com>
2018-10-17 22:02:29 +0200

    change the name of some methods

--- TemchiB3id/src/main/java/Co_Voiturage/TemchiB3id/Repository/UserOffreRepositry.java
+++ TemchiB3id/src/main/java/Co_Voiturage/TemchiB3id/Repository/UserOffreRepositry.java
@@ -9,8 +9,9 @@ import Co_Voiturage.TemchiB3id.Model.Offre_User;
 
 public interface UserOffreRepositry extends JpaRepository<Offre_User, Long> {
 	
-	List<Offre_User> findByOffre(Long id_offre );
-	List<Offre_User> findByUser(Long id_user );
+	public List<Offre_User> findByIdoffre(Long idoffre );
+	public List<Offre_User> findByIduser(Long iduser);
+	//public List<Offre_User> findByUser(Long id_user );
 	
     // les méthodes save(), findOne(), findAll(), count(), delete() sont generées automatiquement par le JPA
 
