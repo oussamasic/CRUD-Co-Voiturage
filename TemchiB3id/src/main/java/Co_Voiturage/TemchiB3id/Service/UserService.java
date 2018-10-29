87287a6f4d776f47e6992179aa5a23cb139278f7
Zerouali <oussamazerouali42@gmail.com>
2018-10-29 22:43:13 +0100

    create user service

---
+++ TemchiB3id/src/main/java/Co_Voiturage/TemchiB3id/Service/UserService.java
@@ -0,0 +1,19 @@
+package Co_Voiturage.TemchiB3id.Service;
+
+import java.util.List;
+
+import Co_Voiturage.TemchiB3id.Model.User;
+
+public interface UserService {
+	
+	public List<User> AllUsers();
+	public List<User> AllDriver();
+	public List<User> AllRequestors();
+	public User GetUser(Long id);
+	public void deleteUser(Long id);
+	public void UpdateUser(User user);
+	public void AddUser(User user);
+	public User UserConnexion(String x, String y);
+	
+
+}
