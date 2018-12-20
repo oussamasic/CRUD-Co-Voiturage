428265d607d6e285dbefd35f2cb35efca8008690
Zerouali <oussamazerouali42@gmail.com>
2018-12-20 15:52:00 +0100

    test

--- TemchiB3id/src/main/java/Co_Voiturage/TemchiB3id/App.java
+++ TemchiB3id/src/main/java/Co_Voiturage/TemchiB3id/App.java
@@ -1,5 +1,9 @@
 package Co_Voiturage.TemchiB3id;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -13,6 +17,32 @@ public class App {
 		SpringApplication.run(App.class, args);
 
 System.out.println("salam");
+//HashMap m = new HashMap();
+//Object o = new Object();
+//Object b = o;
+//m.put(o, 5);
+//m.put(b,6);
+//String s="salam ana oussama zerouali";
+//String[] ch = s.split(" ");
+//for(int i=0;i<ch.length;i++) {
+//	System.out.println("salam" +ch[i]);
+//	
+//}
+//Integer[] table = {1,44,3,99,5};
+//Arrays.sort(table, Collections.reverseOrder());
+//
+//System.out.println("salam"+table[table.length-1]);
+//String f= String.valueOf(table.length);
+////System.out.println("salam" +table[table.length-1]);
+//
+//System.out.println(f+" salam " +m.get(b));
+//
+//for(int i =65;i<=90;i++) {
+//	System.out.println((char)i);
+//}
+		
+	
 
+		
 	}
 }
